b303849e29cc45aef4594b719871a46ac8984df9
John Morsley <johnmorsley@gmail.com>
2020-03-22 21:30:56 +0000

    Added DynamoDB fro Rancher Walking Skeleton.

--- dynamodb.tf
+++ dynamodb.tf
@@ -43,3 +43,17 @@ resource "aws_dynamodb_table" "terraform-statelock-bootstrap-rancher" {
     }
 
 }
+
+resource "aws_dynamodb_table" "terraform-statelock-rancher-walking-skeleton" {
+
+    name = "morsley-io-tfstatelock-bootstrap-rancher"
+    read_capacity = 20
+    write_capacity = 20
+    hash_key = "LockID"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+
+}
