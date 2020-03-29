11ef0b703beab9884e62380165f410d562209c69
John Morsley <johnmorsley@gmail.com>
2020-03-29 18:19:43 +0100

    Added other locks.

---
+++ locks/concourse.tf
@@ -0,0 +1,11 @@
+﻿module "aws-lock-cluster" {
+
+  source = "./modules/aws"
+
+  access_key  = var.access_key
+  secret_key  = var.secret_key
+
+  domain_name = var.domain_name
+  lock_name = "concourse"
+
+}
